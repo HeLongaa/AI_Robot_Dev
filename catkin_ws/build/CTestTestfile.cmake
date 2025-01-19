7e05ad7089156a57ecf0f86e9627e45f466100d1
helong <helong20050823@gmail.com>
2025-01-19 17:34:50 +0800

    Debug the running parameters of the robot and modify the running logic; Added some functions of the AI assistant (Not debug on Pi, only tested locally,The environment is Python 12 On iMac)

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/admin/Desktop/AI_Robot_Dev/catkin_ws/src
+# Build directory: /home/admin/Desktop/AI_Robot_Dev/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("robot_control")
---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/admin/Desktop/AI_Robot_Dev/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/admin/Desktop/AI_Robot_Dev/catkin_ws/devel'.split(';') if '/home/admin/Desktop/AI_Robot_Dev/catkin_ws/devel' != '' else []
