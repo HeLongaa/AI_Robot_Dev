2ba2687e92d249a5b29c05009fbac2f9beab2cc5
Long He <71657914+HeLongaa@users.noreply.github.com>
2025-02-13 00:14:29 +0800

    Added the management interface.

---
+++ web/app.py
@@ -0,0 +1,75 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from models import db, User
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your-secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/docx')
+def docx():
+    return render_template('docx.html')
+
+@app.route('/demo')
+def demo():
+    return render_template('demo.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/reg', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        id = request.form.get('id')
+        
+        if User.query.filter_by(username=username).first():
+            flash('用户名已存在')
+            return redirect(url_for('register'))
+
+        user = User(username=username)
+        user.set_password(password)
+        db.session.add(user)
+        db.session.commit()
+        
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username=username).first()
+        
+        if user and user.check_password(password):
+            login_user(user)
+            return redirect(url_for('manager'))
+        flash('用户名或密码错误')
+    return render_template('login.html')
+
+@app.route('/manager')
+@login_required
+def manager():
+    return render_template('manager.html')
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True) 
--- web/run.py
+++ web/run.py
@@ -1,13 +1,32 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2025/2/3 01:12
+# @Time    : 2025/2/12 22:44
 # @Author  : HeLong
 # @FileName: run.py
 # @Software: PyCharm
 # @Blog    : https://helong.online/
 
-from app import create_app
+from flask import Flask, render_template
+app = Flask(__name__)
 
-app = create_app()
+
+app.register_blueprint(auth, url_prefix='/auth')
+app.register_blueprint(manager, url_prefix='/manager')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/docx')
+def docx():
+    return render_template('docx.html')
+
+@app.route('/demo')
+def demo():
+    return render_template('demo.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
